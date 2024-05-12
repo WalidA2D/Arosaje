42755d52fbc3a07d958582e02e4ecc9fc242a2ec
unknown <youss78540@gmail.com>
2024-05-12 15:00:24 +0200

    correction serrergui

--- App/app/(tabs)/index.tsx
+++ App/app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Ils veluent me faire serrer</ThemedText>
+        <ThemedText type="title">Ils veluent me faire serrergui</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
