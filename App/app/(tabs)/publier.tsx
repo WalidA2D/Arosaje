881cd350c15607a86515d7873dcf36a24d0d3d65
Youssef <youss78540@gmail.com>
2024-05-25 20:14:38 +0200

    Vitrine Publier

--- App/app/(tabs)/publier.tsx
+++ App/app/(tabs)/publier.tsx
@@ -1,88 +1,76 @@
 import React, { useState } from 'react';
 import { StyleSheet, Image, View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function PublierScreen() {
-  const [selectedTab, setSelectedTab] = useState('Posts'); // État pour gérer le sélecteur
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity style={styles.headerButton}>
-          <Text style={styles.headerButtonText}>Options</Text>
-        </TouchableOpacity>
-        <View style={styles.headerTitleContainer}>
-          <Text style={styles.headerText}>Profil</Text>
-        </View>
-        <TouchableOpacity style={styles.headerButton}>
-          <Text style={styles.headerButtonText}>Déconnexion</Text>
-        </TouchableOpacity>
+    <View style={styles.header}>
+      <View style={styles.headerTitleContainer}>
+        <Text style={styles.headerText}>Publier</Text>
       </View>
-      
-      <View style={styles.profileImageContainer}>
-        <Image
-          source={require('@/assets/images/pp_base.jpg')}
-          style={styles.profileImage}
-        />
-      </View>
-      
-      <View style={styles.fixedDetails}>
-        <View style={styles.profileDetails}>
-          <Text style={styles.profileName}>Victoria Robertson</Text>
-          <Text style={styles.profileRole}>Rôle | Localisation</Text>
-        </View>
-        
-        <View style={styles.selectorContainer}>
-          <TouchableOpacity 
-            style={[styles.selectorButton, selectedTab === 'Posts' && styles.activeButton]} 
-            onPress={() => setSelectedTab('Posts')}
-          >
-            <Text style={[styles.selectorText, selectedTab === 'Posts' && styles.activeText]}>Posts</Text>
-          </TouchableOpacity>
-          <TouchableOpacity 
-            style={[styles.selectorButton, selectedTab === 'Images' && styles.activeButton]} 
-            onPress={() => setSelectedTab('Images')}
-          >
-            <Text style={[styles.selectorText, selectedTab === 'Images' && styles.activeText]}>Images</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      
-      <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        <View style={styles.body}>
-          {/* Contenu affiché en fonction du sélecteur */}
-          {selectedTab === 'Posts' ? (
-            <View>
-              <View style={styles.post}>
-                <Text style={styles.postTitle}>Header</Text>
-                <Text style={styles.postContent}>He'll want to use your yacht, and I don't want this thing smelling like fish.</Text>
-              </View>
-              <View style={styles.post}>
-                <Text style={styles.postTitle}>Header</Text>
-                <Text style={styles.postContent}>He'll want to use your yacht, and I don't want this thing smelling like fish.</Text>
-              </View>
-              <View style={styles.post}>
-                <Text style={styles.postTitle}>Header</Text>
-                <Text style={styles.postContent}>He'll want to use your yacht, and I don't want this thing smelling like fish.</Text>
-              </View>
-              {/* Ajoutez d'autres posts ici */}
-            </View>
-          ) : (
-            <View>
-              <View style={styles.image}>
-                <Image source={require('@/assets/images/plante1.jpg')} style={styles.imageContent} />
-              </View>
-              <View style={styles.image}>
-                <Image source={require('@/assets/images/plante2.jpg')} style={styles.imageContent} />
-              </View>
-              <View style={styles.image}>
-                <Image source={require('@/assets/images/plante3.jpg')} style={styles.imageContent} />
-              </View>
-              {/* Ajoutez d'autres images ici */}
-            </View>
-          )}
-        </View>
-      </ScrollView>
     </View>
+    
+    <View style={styles.fixedDetails}>
+      <TouchableOpacity style={[styles.selectorOptions, { width: '100%' }]}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+              <Text style={styles.selectorOptionsText}>Titre</Text>
+              <Ionicons name="chevron-forward-outline" size={18} color="black" />
+          </View>
+      </TouchableOpacity>
+      <View style={styles.separatorDetails}/>
+      <TouchableOpacity style={[styles.selectorOptions, { width: '100%' }]}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+              <Text style={styles.selectorOptionsText}>Date(s)</Text>
+              <Ionicons name="chevron-forward-outline" size={18} color="black" />
+          </View>
+      </TouchableOpacity>
+      <View style={styles.separatorDetails}/>
+      <TouchableOpacity style={[styles.selectorOptions, { width: '100%' }]}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+              <Text style={styles.selectorOptionsText}>Photo(s)</Text>
+              <Ionicons name="chevron-forward-outline" size={18} color="black" />
+          </View>
+      </TouchableOpacity>
+      <View style={styles.separatorDetails}/>
+      <TouchableOpacity style={[styles.selectorOptions, { width: '100%' }]}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+              <Text style={styles.selectorOptionsText}>Description</Text>
+              <Ionicons name="chevron-forward-outline" size={18} color="black" />
+          </View>
+      </TouchableOpacity>
+      <View style={styles.separatorDetails}/>
+      <TouchableOpacity style={[styles.selectorOptions, { width: '100%' }]}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+              <Text style={styles.selectorOptionsText}>Localisation</Text>
+              <Ionicons name="chevron-forward-outline" size={18} color="black" />
+          </View>
+      </TouchableOpacity>
+      <View style={styles.separatorDetails}/>
+      <TouchableOpacity style={[styles.selectorOptions, { width: '100%' }]}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+              <Text style={styles.selectorOptionsText}>Espèce(s)</Text>
+              <Ionicons name="chevron-forward-outline" size={18} color="black" />
+          </View>
+      </TouchableOpacity>
+      <View style={styles.separatorDetails}/>
+      <TouchableOpacity style={[styles.selectorOptions, { width: '100%' }]}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+              <Text style={styles.selectorOptionsText}>Exigence d'entretien (optionel)</Text>
+              <Ionicons name="chevron-forward-outline" size={18} color="black" />
+          </View>
+      </TouchableOpacity>
+      <View style={styles.separatorDetails}/>
+    </View>
+    <View style={styles.fixedDetailsBtn}>
+      <View style={styles.selectorContainer}>
+          <TouchableOpacity style={styles.selectorButton}>
+              <Text style={{color : '#FFF', fontSize : 14, fontWeight: 'bold',}}>Valider</Text>
+          </TouchableOpacity>
+      </View>
+      </View>
+  </View>
   );
 }
 
@@ -92,7 +80,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF', 
   },
   header: {
-    height: 200,
+    height: 100,
     backgroundColor: '#668F80',
     flexDirection: 'row',
     alignItems: 'center',
@@ -112,45 +100,14 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
-  headerButton: {
-    padding: 10,
-    paddingBottom: 50,
-  },
-  headerButtonText: {
-    color: '#FFF',
-    fontSize: 16,
-    paddingBottom: 50,
-  },
-  profileImageContainer: {
-    position: 'absolute',
-    top: 100,
-    left: 0,
-    right: 0,
-    alignItems: 'center',
-    zIndex: 1,
-  },
-  profileImage: {
-    width: 120,
-    height: 120,
-    borderRadius: 60,
-    borderWidth: 3,
-    borderColor: '#FFF', 
-  },
   fixedDetails: {
-    marginTop: 40,
+    marginTop: 30,
+    alignItems: 'flex-start',
+  },
+  fixedDetailsBtn: {
+    paddingTop : 100,
+    marginTop: 100,
     alignItems: 'center',
-  },
-  profileDetails: {
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  profileName: {
-    fontSize: 22,
-    fontWeight: 'bold',
-  },
-  profileRole: {
-    fontSize: 16,
-    color: 'gray',
   },
   scrollViewContent: {
     paddingTop: 20, 
@@ -168,41 +125,27 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     overflow: 'hidden',
     width: '90%', 
+    alignItems: 'center',
+  },
+  separatorDetails: {
+    height: 1,
+    backgroundColor: '#E8E8E8',
+    width: '95%',
+    marginVertical: 5,
+    alignSelf : 'center',
   },
   selectorButton: {
     flex: 1,
-    paddingVertical: 10,
+    paddingVertical: 20,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  activeButton: {
     backgroundColor: '#668F80',
   },
-  selectorText: {
-    fontSize: 16,
-    color: '#666',
+  selectorOptions: {
+    padding : 10,
+    color : '#BDBDBD',
   },
-  activeText: {
-    color: '#FFF',
-    fontWeight: 'bold',
-  },
-  post: {
-    marginBottom: 20,
-  },
-  postTitle: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  postContent: {
-    fontSize: 14,
-    color: 'gray',
-  },
-  image: {
-    marginBottom: 20,
-  },
-  imageContent: {
-    width: '100%',
-    height: 200,
-    resizeMode: 'cover',
-  },
+  selectorOptionsText: {
+    fontSize : 14,
+  }
 });
