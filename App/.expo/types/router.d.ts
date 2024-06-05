c559e39937afeebd5c0c3ea5b2d0cb6ae774456d
Walid <waitdaoud@dailybiz.com>
2024-06-05 15:16:31 +0200

    Mise en place du front

--- App/.expo/types/router.d.ts
+++ App/.expo/types/router.d.ts
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 import type { TextProps, GestureResponderEvent } from 'react-native';
 
 export namespace ExpoRouter {
-  type StaticRoutes = `/` | `/(tabs)` | `/..\components\HeaderTitle` | `/..\components\ListDash` | `/_sitemap` | `/calendrier` | `/explore` | `/loginform` | `/options` | `/profil` | `/publier`;
+  type StaticRoutes = `/` | `/(tabs)` | `/_sitemap` | `/calendrier` | `/explore` | `/loginform` | `/message` | `/options` | `/profil` | `/publier`;
   type DynamicRoutes<T extends string> = never;
   type DynamicRouteTemplate = never;
 
