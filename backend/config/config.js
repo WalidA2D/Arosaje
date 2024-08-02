c147aadd883097f0e41d2f09ab2a97968f1c79e2
Massil <amirat.massil@gmail.com>
2024-08-02 20:02:43 +0200

    Suppression dossier "backend"

--- backend/config/config.js
+++
@@ -1,7 +0,0 @@
-//ce fichier sert à garder nos données sensibles et ne pas les exposer dans les fichiers partagés avec le client.
-require('dotenv').config(); // Charger les variables d'environnement
-
-module.exports = {
-    PORT: process.env.PORT || 3000, // Port du serveur
-    DATABASE_URL: process.env.DATABASE_URL // URL de la base de données
-};
