6bf9f601762e582ae444f6c24927906351fb57c2
handsomeAymenn <aymenboushabi@outlook.fr>
2024-08-03 18:07:07 +0200

    récup des postes via l'API sur accueil

--- App/app/(tabs)/actu.tsx
+++ App/app/(tabs)/actu.tsx
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Button, ScrollView, TextInput } from 'react-native';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { StyleSheet, View, FlatList, ActivityIndicator, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import ContentItem from '../../components/navigation/ContentItem';
-import Filtre from '../actunav/actufiltre';
+
+type Post = {
+  idPosts: number;
+  title: string;
+  description: string;
+  publishedAt: Date;
+  image1: string | null;
+  image2: string | null;
+  image3: string | null;
+};
+
+type ContentItemData = {
+  id: string;
+  images: string[];
+  title: string;
+  description: string;
+  time: string;
+};
 
 type RootStackParamList = {
   Actualités: undefined;
@@ -12,86 +29,125 @@ type RootStackParamList = {
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+const apiUrl = process.env.EXPO_PUBLIC_API_IP;
 
-function HomeScreen({ }) {
+function HomeScreen() {
   return (
     <NavigationContainer independent={true}>
-    <Stack.Navigator
-      initialRouteName="Actualités"
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#668F80',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          color: '#FFF',
-          fontSize: 24,
-          fontWeight: 'bold',
-        },
-      }}
-    >
-    <Stack.Screen
-      name="Actualités"
-      component={HomeContent}
-      options={({ navigation }) => ({
-        headerRight: () => (
-          <Button
-            onPress={() => navigation.navigate('Filtre')}
-            title="Filtre"
-            color="#fff"
-          />
-        ),
-      })}
-    />
-    <Stack.Screen
-      name="Filtre"
-      component={Filtre}
-      options={{ headerBackTitleVisible: false }}
-    />
+      <Stack.Navigator
+        initialRouteName="Actualités"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#668F80',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            color: '#FFF',
+            fontSize: 24,
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Actualités"
+          component={HomeContent}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 function HomeContent() {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [items, setItems] = useState<ContentItemData[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+  const [saut, setSaut] = useState<number>(0);
+  const quantite = 5;
+
+  const fetchPosts = async () => {
+    if (loading) return; // Éviter de lancer plusieurs requêtes simultanément
+  
+    setLoading(true);
+  
+    try {
+      await new Promise(resolve => setTimeout(resolve, 2000));
+  
+      const response = await fetch(`${apiUrl}/post/read?quantite=${quantite}&saut=${saut}`);
+      console.log(response);
+      console.log(`${apiUrl}/post/read?quantite=${quantite}&saut=${saut}`);
+  
+      if (!response.ok) {
+        throw new Error('Échec de la réponse du serveur');
+      }
+  
+      const result = await response.json();
+  
+      if (Array.isArray(result.posts)) {
+        if (result.posts.length === 0) {
+          if (items.length > 0 && !error) {
+            setError('Aucun poste supplémentaire à charger.');
+          }
+        } else {
+          const newItems = result.posts.map((post: Post) => ({
+            id: post.idPosts.toString(),
+            images: [post.image1, post.image2, post.image3].filter(Boolean) as string[],
+            title: post.title,
+            description: post.description,
+            time: new Date(post.publishedAt).toLocaleTimeString(),
+          }));
+  
+          setItems((prevItems) => [...prevItems, ...newItems]);
+          setSaut((prevSaut) => prevSaut + quantite);
+          setError('');
+        }
+      } else {
+        console.error('La réponse du serveur ne contient pas de tableau de posts');
+        setError('Erreur lors de la récupération des postes');
+      }
+    } catch (error) {
+      console.error('Erreur lors de la récupération des postes:', error);
+      setError("Impossible de charger les postes. Veuillez vérifier votre connexion et réessayer.");
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+  
+  
+
+  const loadMoreItems = () => {
+    if (!loading && !error) {
+      fetchPosts();
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   return (
-      <View style={styles.container}>
-
-        <View style={styles.searchContainer}>
-          <TextInput
-            style={styles.searchInput}
-            placeholder="Rechercher..."
-            placeholderTextColor="#888"
-            value={searchQuery}
-            onChangeText={(text) => setSearchQuery(text)}
-          />
-        </View>
-
-        <ScrollView contentContainerStyle={styles.scrollViewContent}>
-          <View style={styles.body}>
+    <View style={styles.container}>
+      {error ? (
+        <Text style={styles.errorText}>{error}</Text>
+      ) : (
+        <FlatList
+          contentContainerStyle={styles.scrollViewContent}
+          data={items}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
             <ContentItem
-              images={[
-                'https://picsum.photos/600/300',
-                'https://picsum.photos/610/300',
-                'https://picsum.photos/620/300',
-              ]}
-              title="Titre du slider"
-              description="Description du slider"
-              time="Il y a 8 minutes"
+              images={item.images}
+              title={item.title}
+              description={item.description}
+              time={item.time}
             />
-            <ContentItem
-              images={[
-                'https://picsum.photos/590/300',
-              ]}
-              title="Titre du slider"
-              description="Description du slider"
-              time="Il y a 18 minutes"
-            />
-          </View>
-        </ScrollView>
-      </View>
+          )}
+          onEndReached={loadMoreItems}
+          onEndReachedThreshold={0.5}
+          ListFooterComponent={loading ? <ActivityIndicator size="large" color="#668F80" /> : null}
+        />
+      )}
+    </View>
   );
 }
 
@@ -100,28 +156,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFF',
   },
-  searchContainer: {
-    marginTop: 10,
-    paddingHorizontal: 20,
-    paddingBottom: 10,
-  },
-  searchInput: {
-    height: 40,
-    borderColor: '#CCC',
-    borderWidth: 1,
-    borderRadius: 20,
-    paddingLeft: 15,
-    backgroundColor: '#F0F0F0',
-    color: '#000',
-  },
   scrollViewContent: {
     paddingTop: 20,
-  },
-  body: {
-    flex: 1,
-    width: '100%',
     paddingHorizontal: 20,
-    backgroundColor: '#FFF',
+  },
+  errorText: {
+    textAlign: 'center',
+    color: 'red',
+    fontSize: 16,
+    margin: 20,
   },
 });
 
