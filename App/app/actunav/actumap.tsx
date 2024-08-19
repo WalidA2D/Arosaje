c39c0d05dcb613f7f8de66a141a3eee7f30a30b5
Youssef EL-ABDI <yelabdi@dailybiz.com>
2024-08-19 17:30:09 +0200

    Map mais icone pas cliquable

--- App/app/actunav/actumap.tsx
+++ App/app/actunav/actumap.tsx
@@ -1,15 +1,27 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import * as Location from 'expo-location';
+import axios from 'axios';
 
 import OpenLayersMap from '../../components/OpenLayersMap';
 import Loading from '../../components/Loading';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function ActuMap() {
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
-  const [loading, setLoading] = useState(false); // Ajout de l'état de chargement
+  const [loading, setLoading] = useState(false);
+  const [markers, setMarkers] = useState<{ latitude: number, longitude: number }[]>([]);
   const mapRef = useRef<any>(null);
+  const apiUrl = process.env.EXPO_PUBLIC_API_IP || '';
+
+  // Add a state to store the selected marker
+  const [selectedMarker, setSelectedMarker] = useState<{ latitude: number, longitude: number } | null>(null);
+
+  // Update the handleMarkerPress function to set the selected marker
+  const handleMarkerPress = (marker: { latitude: number, longitude: number }) => {
+    setSelectedMarker(marker);
+  };
 
   useEffect(() => {
     (async () => {
@@ -21,17 +33,71 @@ export default function ActuMap() {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+
+      // Fetch user posts to get markers
+      fetchUserPosts();
     })();
   }, []);
 
+  const fetchUserPosts = async () => {
+    const userToken = await AsyncStorage.getItem('userToken');
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': userToken || '',
+        }
+    };
+
+    try {
+        const response = await fetch(`${apiUrl}/post/read`, options);
+        const data = await response.json();
+
+        if (data.success && data.posts) {
+            const filteredPosts = data.posts.filter(post => !post.accepted && post.acceptedBy === null);
+            const markersData = await Promise.all(filteredPosts.map(async (post: { address: string, cityName: string }) => {
+                const address = post.address;
+                const cityName = post.cityName;
+                const coordinates = await fetchCoordinates(address, cityName);
+                return coordinates;
+            }));
+            setMarkers(markersData);
+        } else {
+            console.error('Failed to fetch posts:', data.message);
+        }
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+    }
+  };
+
+  const fetchCoordinates = async (address: string, cityName: string) => {
+    const formattedAddress = `${address}, ${cityName}`;
+    const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(formattedAddress)}&format=json`;
+
+    try {
+        const response = await axios.get(url);
+        
+        if (response.data && response.data.length > 0) {
+            const { lat, lon } = response.data[0];
+            return { latitude: lat, longitude: lon };
+        } else {
+            console.error('No results found for the address:', formattedAddress);
+            return null;
+        }
+    } catch (error) {
+        console.error('Error fetching coordinates:', error);
+        return null;
+    }
+  };
+
   const recenterMap = async () => {
-    setLoading(true); // Début du chargement
+    setLoading(true);
     let location = await Location.getCurrentPositionAsync({});
     setLocation(location);
     if (mapRef.current) {
       (mapRef.current as any).setCenter([location.coords.longitude, location.coords.latitude]);
     }
-    setLoading(false); // Fin du chargement
+    setLoading(false);
   };
 
   return (
@@ -42,7 +108,8 @@ export default function ActuMap() {
             ref={mapRef}
             latitude={location.coords.latitude}
             longitude={location.coords.longitude}
-            forwardRef={mapRef}
+            markers={markers}
+            onMarkerPress={handleMarkerPress} // Pass the handleMarkerPress function to the map component
           />
         ) : (
           <Loading />
@@ -50,7 +117,7 @@ export default function ActuMap() {
       </View>
       <TouchableOpacity style={styles.button} onPress={recenterMap} disabled={loading}>
         {loading ? (
-          <Loading /> // Affichage du composant de chargement
+          <Loading />
         ) : (
           <Ionicons name="navigate-outline" size={32} color="#fff" />
         )}
--- App/components/OpenLayersMap.js
+++ App/components/OpenLayersMap.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
 
-const OpenLayersMap = ({ latitude, longitude }) => {
+const OpenLayersMap = ({ latitude, longitude, markers, onMarkerPress }) => {
+  const markersJSArray = JSON.stringify(markers);
+
   const html = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>OpenLayers Map</title>
-      <link rel="stylesheet" href="https://openlayers.org/en/v6.5.0/css/ol.css" type="text/css">
-      <style>
+<!DOCTYPE html>
+<html>
+<head>
+<title>OpenLayers Map</title>
+<link rel="stylesheet" href="https://openlayers.org/en/v6.5.0/css/ol.css" type="text/css">
+<style>
         #map {
           width: 100%;
           height: 100%;
@@ -16,12 +18,12 @@ const OpenLayersMap = ({ latitude, longitude }) => {
           top: 0;
           left: 0;
         }
-      </style>
-      <script src="https://openlayers.org/en/v6.5.0/build/ol.js"></script>
-    </head>
-    <body>
-      <div id="map" class="map"></div>
-      <script type="text/javascript">
+</style>
+<script src="https://openlayers.org/en/v6.5.0/build/ol.js"></script>
+</head>
+<body>
+<div id="map" class="map"></div>
+<script type="text/javascript">
         document.addEventListener('DOMContentLoaded', function() {
           var map = new ol.Map({
             target: 'map',
@@ -32,18 +34,18 @@ const OpenLayersMap = ({ latitude, longitude }) => {
             ],
             view: new ol.View({
               center: ol.proj.fromLonLat([${longitude}, ${latitude}]),
-              zoom: 18 // Increase zoom level for a closer view
+              zoom: 18 
             })
           });
-
-          var radius = 100; // Radius in meters
+ 
+          var radius = 100;
           var circle = new ol.geom.Circle(ol.proj.fromLonLat([${longitude}, ${latitude}]), radius);
           var circleFeature = new ol.Feature(circle);
-
+ 
           var vectorSource = new ol.source.Vector({
             features: [circleFeature]
           });
-
+ 
           var vectorLayer = new ol.layer.Vector({
             source: vectorSource,
             style: new ol.style.Style({
@@ -56,12 +58,37 @@ const OpenLayersMap = ({ latitude, longitude }) => {
               })
             })
           });
-
+ 
           map.addLayer(vectorLayer);
+ 
+          var iconStyle = new ol.style.Style({
+            image: new ol.style.Icon({
+              anchor: [0.5, 1],
+              src: 'https://png.pngtree.com/png-clipart/20230410/ourmid/pngtree-go-green-little-plant-png-image_6698190.png',
+              scale: 0.5
+            })
+          });
+ 
+          var markers = ${markersJSArray};
+ 
+          markers.forEach(function(marker) {
+            var iconFeature = new ol.Feature({
+              geometry: new ol.geom.Point(ol.proj.fromLonLat([marker.longitude, marker.latitude]))
+            });
+ 
+            iconFeature.setStyle(iconStyle);
+ 
+            // Add click event listener to the marker
+            iconFeature.on('click', function() {
+              onMarkerPress(marker);
+            });
+ 
+            vectorSource.addFeature(iconFeature);
+          });
         });
-      </script>
-    </body>
-    </html>
+</script>
+</body>
+</html>
   `;
 
   return (
